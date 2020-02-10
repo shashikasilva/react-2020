c8055e68b04f1119e3db7367eccc149ec583a193
Shashika Kaveendra Silva <shashika.silva88@gmail.com>
2020-02-10 14:24:34 +0530

    Update unsplash.js

--- pics/src/api/unsplash.js
+++ pics/src/api/unsplash.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID xxx'
+        Authorization: 'Client-ID 02bf5961d8778b8bdb75617158a080555bcc443a120befe7f8a29e4b5d8e3211'
     }
 
 });
